c8379521520c79e90f7604e8adf42072d91a30b2
harveyrodchristian <valmera27@gmail.com>
2025-09-28 22:54:47 +0800

    Add files via upload

---
+++ Minglanilla Store/version 3.0/app.js
@@ -0,0 +1,4 @@
+document.getElementById('addProductBtn').addEventListener('click', () => {
+    document.getElementById('addProductModal').style.display = 'block';
+    document.getElementById('productName').focus();
+});
